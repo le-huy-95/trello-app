eafe9bffb88b94dab3545a04b2c615021cea1403
lexuanhuy <Lexuanhuy8695@gmail.com>
2022-10-10 16:54:22 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,9 +1,231 @@
-import "./App.css";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello huy</h1>
+    <div className="App-container">
+      <nav className="navbar app"> app name</nav>
+      <nav className="navbar board"> board bar</nav>
+      <div className="board-content">
+        <div className="column">
+          <header> Columns Tab </header>
+          <ul>
+            <li>
+              <img
+                src="https://cdn.tgdd.vn/Files/2016/02/25/792452/chup-anh-hoang-hon-bang-smartphone_800x450.jpg"
+                alt=""
+              />{" "}
+            </li>
+
+            <li>title: your'name</li>
+            <li>Add what you'd like to work on below </li>
+            <li>Add what you'd like to work on below </li>
+            <li>Add what you'd like to work on below </li>
+            <li>Add what you'd like to work on below </li>
+            <li>Add what you'd like to work on below </li>
+            <li>Add what you'd like to work on below </li>
+
+            <li>Add what you'd like to work on below </li>
+
+            <li>Add what you'd like to work on below </li>
+
+            <li>Add what you'd like to work on below </li>
+
+            <li>Add what you'd like to work on below </li>
+
+            <li>Add what you'd like to work on below </li>
+
+            <li>Add what you'd like to work on below </li>
+
+            <li>Add what you'd like to work on below </li>
+
+            <li>Add what you'd like to work on below </li>
+
+            <li>Add what you'd like to work on below </li>
+
+            <li>Add what you'd like to work on below </li>
+
+            <li>Add what you'd like to work on below </li>
+
+            <li>Add what you'd like to work on below </li>
+
+            <li>Add what you'd like to work on below </li>
+
+            <li>Add what you'd like to work on below </li>
+          </ul>
+          <footer> Add another card</footer>
+        </div>
+        <div className="column">
+          <header> Columns Tab </header>
+          <ul>
+            <li>
+              <img
+                src="https://cdn.tgdd.vn/Files/2016/02/25/792452/chup-anh-hoang-hon-bang-smartphone_800x450.jpg"
+                alt=""
+              />{" "}
+            </li>
+
+            <li>title: your'name</li>
+            <li>Add what you'd like to work on below </li>
+            <li>Add what you'd like to work on below </li>
+            <li>Add what you'd like to work on below </li>
+            <li>Add what you'd like to work on below </li>
+            <li>Add what you'd like to work on below </li>
+          </ul>
+          <footer> Add another card</footer>
+        </div>
+        <div className="column">
+          <header> Columns Tab </header>
+          <ul>
+            <li>
+              <img
+                src="https://cdn.tgdd.vn/Files/2016/02/25/792452/chup-anh-hoang-hon-bang-smartphone_800x450.jpg"
+                alt=""
+              />{" "}
+            </li>
+
+            <li>title: your'name</li>
+            <li>Add what you'd like to work on below </li>
+            <li>Add what you'd like to work on below </li>
+            <li>Add what you'd like to work on below </li>
+            <li>Add what you'd like to work on below </li>
+            <li>Add what you'd like to work on below </li>
+          </ul>
+          <footer> Add another card</footer>
+        </div>
+        <div className="column">
+          <header> Columns Tab </header>
+          <ul>
+            <li>
+              <img
+                src="https://cdn.tgdd.vn/Files/2016/02/25/792452/chup-anh-hoang-hon-bang-smartphone_800x450.jpg"
+                alt=""
+              />{" "}
+            </li>
+
+            <li>title: your'name</li>
+            <li>Add what you'd like to work on below </li>
+            <li>Add what you'd like to work on below </li>
+            <li>Add what you'd like to work on below </li>
+            <li>Add what you'd like to work on below </li>
+            <li>Add what you'd like to work on below </li>
+          </ul>
+          <footer> Add another card</footer>
+        </div>
+        <div className="column">
+          <header> Columns Tab </header>
+          <ul>
+            <li>
+              <img
+                src="https://cdn.tgdd.vn/Files/2016/02/25/792452/chup-anh-hoang-hon-bang-smartphone_800x450.jpg"
+                alt=""
+              />{" "}
+            </li>
+
+            <li>title: your'name</li>
+            <li>Add what you'd like to work on below </li>
+            <li>Add what you'd like to work on below </li>
+            <li>Add what you'd like to work on below </li>
+            <li>Add what you'd like to work on below </li>
+            <li>Add what you'd like to work on below </li>
+          </ul>
+          <footer> Add another card</footer>
+        </div>
+        <div className="column">
+          <header> Columns Tab </header>
+          <ul>
+            <li>
+              <img
+                src="https://cdn.tgdd.vn/Files/2016/02/25/792452/chup-anh-hoang-hon-bang-smartphone_800x450.jpg"
+                alt=""
+              />{" "}
+            </li>
+
+            <li>title: your'name</li>
+            <li>Add what you'd like to work on below </li>
+            <li>Add what you'd like to work on below </li>
+            <li>Add what you'd like to work on below </li>
+            <li>Add what you'd like to work on below </li>
+            <li>Add what you'd like to work on below </li>
+          </ul>
+          <footer> Add another card</footer>
+        </div>
+        <div className="column">
+          <header> Columns Tab </header>
+          <ul>
+            <li>
+              <img
+                src="https://cdn.tgdd.vn/Files/2016/02/25/792452/chup-anh-hoang-hon-bang-smartphone_800x450.jpg"
+                alt=""
+              />{" "}
+            </li>
+
+            <li>title: your'name</li>
+            <li>Add what you'd like to work on below </li>
+            <li>Add what you'd like to work on below </li>
+            <li>Add what you'd like to work on below </li>
+            <li>Add what you'd like to work on below </li>
+            <li>Add what you'd like to work on below </li>
+          </ul>
+          <footer> Add another card</footer>
+        </div>
+        <div className="column">
+          <header> Columns Tab </header>
+          <ul>
+            <li>
+              <img
+                src="https://cdn.tgdd.vn/Files/2016/02/25/792452/chup-anh-hoang-hon-bang-smartphone_800x450.jpg"
+                alt=""
+              />{" "}
+            </li>
+
+            <li>title: your'name</li>
+            <li>Add what you'd like to work on below </li>
+            <li>Add what you'd like to work on below </li>
+            <li>Add what you'd like to work on below </li>
+            <li>Add what you'd like to work on below </li>
+            <li>Add what you'd like to work on below </li>
+          </ul>
+          <footer> Add another card</footer>
+        </div>
+        <div className="column">
+          <header> Columns Tab </header>
+          <ul>
+            <li>
+              <img
+                src="https://cdn.tgdd.vn/Files/2016/02/25/792452/chup-anh-hoang-hon-bang-smartphone_800x450.jpg"
+                alt=""
+              />{" "}
+            </li>
+
+            <li>title: your'name</li>
+            <li>Add what you'd like to work on below </li>
+            <li>Add what you'd like to work on below </li>
+            <li>Add what you'd like to work on below </li>
+            <li>Add what you'd like to work on below </li>
+            <li>Add what you'd like to work on below </li>
+          </ul>
+          <footer> Add another card</footer>
+        </div>
+        <div className="column">
+          <header> Columns Tab </header>
+          <ul>
+            <li>
+              <img
+                src="https://cdn.tgdd.vn/Files/2016/02/25/792452/chup-anh-hoang-hon-bang-smartphone_800x450.jpg"
+                alt=""
+              />{" "}
+            </li>
+
+            <li>title: your'name</li>
+            <li>Add what you'd like to work on below </li>
+            <li>Add what you'd like to work on below </li>
+            <li>Add what you'd like to work on below </li>
+            <li>Add what you'd like to work on below </li>
+            <li>Add what you'd like to work on below </li>
+          </ul>
+          <footer> Add another card</footer>
+        </div>
+      </div>
     </div>
   );
 }
